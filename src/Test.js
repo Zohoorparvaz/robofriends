2ec4b69c075815b5d3482a07d6854ac63e17fdab
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-23 00:37:50 -0700

    reset the project

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import { Component } from "react"
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="f1 tc">
+        <h1>Testig Classes</h1>
+        <p>continue testing</p>
+      </div>
+    )
+  }
+}
+
+export default Test;
