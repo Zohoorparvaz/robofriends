325e68091c8115d0560a31ec69c280f25da1e493
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-24 15:47:49 -0700

    fetch robots from API + useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cardlist from "./components/Cardlist"
-import { robots } from "./DB/robots"
+// import { robots } from "./DB/robots"
 import Searchbox from "./components/Searchbox"
 
 
 const App = () => {
+  const [data, setData] = useState([])
   const [robot, setRobot] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
+      setData(result)
+    }
+    fetchData()
+  }, [])
+
   const onSearch = (e) => {
     setRobot(e.target.value);
   }
-  let filteredRobots = robots.filter(bot => {
+  let filteredRobots = data.filter(bot => {
     return bot.name.toLowerCase().includes(robot.toLowerCase())
   });
   return (
